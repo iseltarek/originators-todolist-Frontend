c259b561faa9c1dae8d021ad2b8508274da124d6
isel tarek <iseltarek15@gmail.com>
2025-02-28 22:30:39 +0200

    initial commit for strucutring the project.

---
+++ src/app/Core/services/auth.service/auth.service.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth.service',
+  imports: [],
+  templateUrl: './auth.service.component.html',
+  styleUrl: './auth.service.component.css'
+})
+export class AuthServiceComponent {
+
+}
