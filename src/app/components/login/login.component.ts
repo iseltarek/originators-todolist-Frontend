83568a21e939f59435e83f7276a5fd1e6c08596b
isel tarek <iseltarek15@gmail.com>
2025-03-20 03:42:02 +0200

    updated login component to be separate component.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,35 +1,66 @@
-import { Component, signal } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { AuthService } from '../../services/auth.service.component';
 import { Router, RouterLink } from '@angular/router';
 import { AuthGuard } from '../../gurds/auth.guard';
 import { User } from '../../models/user.model';
 import { AuthFormComponent } from '../auth-form/auth-form.component';
+import { AntdModule } from '../../modules/antd.module';
+import { MaterialssModule } from '../../modules/material.module';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [AuthFormComponent],
+  imports: [AntdModule, MaterialssModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrl: './login.component.less',
 })
 export class LoginComponent {
-  errorMessage = signal<string | null>(null);
+  errorMessage = '';
+  hide = '';
+  loginForm: FormGroup;
 
   constructor(
-    public authService: AuthService,
+    private fb: FormBuilder,
     public router: Router,
+    public authService: AuthService,
     public authGuard: AuthGuard
-  ) {}
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 
-  login(loginForm: User) {
+  login() {
     this.authService
-      .login(loginForm.email as string, loginForm.password as string)
+      .login(
+        this.loginForm.value.email as string,
+        this.loginForm.value.password as string
+      )
       .subscribe({
         next: () => {
           this.router.navigate(['/home']);
         },
         error: (err) => {
-          this.errorMessage.set(err.error.message);
+          this.errorMessage = err.error.message;
         },
       });
+  }
+  navigateToSignup() {
+    this.router.navigate(['/landingpage/signup']);
+  }
+
+  togglePasswordVisibility(event: MouseEvent) {
+    // this.hide.set(!this.hide());
+    // event.stopPropagation();
   }
 }
